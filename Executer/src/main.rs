d8fd63491d4bfacd3b9be758423802dc17352668
LiuHaolan <3140101098@zju.edu.cn>
2021-03-02 20:18:24 -0800

    base

--- Executer/src/main.rs
+++ Executer/src/main.rs
@@ -7,22 +7,86 @@ use {
         future::Future,
         sync::mpsc::{sync_channel, Receiver, SyncSender},
         sync::{Arc, Mutex},
-        task::{Context, Poll},
+        task::{Context, Poll,Waker},
         time::Duration,
     },
     // The timer we wrote in the previous section:
 //    timer_future::TimerFuture,
 };
 
+use std::sync::mpsc;
+
+use std::thread;
+
+use std::pin::Pin;
 use futures::executor::block_on;
 
+//use crate::dummy_smol;
+
+/*
 async fn hello_world() {
 
 println!("Hello, world!");
 }
+*/
+
+pub struct TimerFuture {
+    shared_state: Arc<Mutex<SharedState>>,
+}
+
+/// Shared state between the future and the waiting thread
+struct SharedState {
+    /// Whether or not the sleep time has elapsed
+    completed: bool,
+
+    /// The waker for the task that `TimerFuture` is running on.
+    /// The thread can use this after setting `completed = true` to tell
+    /// `TimerFuture`'s task to wake up, see that `completed = true`, and
+    /// move forward.
+    waker: Option<Waker>,
+}
+
+impl Future for TimerFuture {
+    type Output = ();
+    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        // Look at the shared state to see if the timer has already completed.
+        let mut shared_state = self.shared_state.lock().unwrap();
+        if shared_state.completed {
+            Poll::Ready(())
+        } else {
+            // Set waker so that the thread can wake up the current task
+            // when the timer has completed, ensuring that the future is polled
+            // again and sees that `completed = true`.
+            //
+            // It's tempting to do this once rather than repeatedly cloning
+            // the waker each time. However, the `TimerFuture` can move between
+            // tasks on the executor, which could cause a stale waker pointing
+            // to the wrong task, preventing `TimerFuture` from waking up
+            // correctly.
+            //
+            // N.B. it's possible to check for this using the `Waker::will_wake`
+            // function, but we omit that here to keep things simple.
+            shared_state.waker = Some(cx.waker().clone());
+            Poll::Pending
+        }
+    }
+}
+
 
 fn main() {
 //    println!("Hello, world!");
+/*
     let f1 = hello_world();
     block_on(f1);
+
+    let( tx, rx) = mpsc::channel();
+     thread::spawn(move || {
+        let val = String::from("hi");
+        tx.send(val).unwrap();
+    });
+
+    let received = rx.recv().unwrap();
+    println!("Got: {}", received);
+*/
+
 }
